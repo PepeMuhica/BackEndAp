d96b7047721b24d0a7adcd0717bc04e2d5328206
juan <developerMuhica@gmail.com>
2023-04-28 00:20:10 -0300

    BackEndHeroku2

--- src/main/java/com/portfolio/cmgm/Controller/CAcercade.java
+++ src/main/java/com/portfolio/cmgm/Controller/CAcercade.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acercademi")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fronendap3.web.app")
 public class CAcercade {
 
     @Autowired
--- src/main/java/com/portfolio/cmgm/Controller/CHSSkill.java
+++ src/main/java/com/portfolio/cmgm/Controller/CHSSkill.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/skills")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fronendap3.web.app")
 public class CHSSkill {
     @Autowired
     SHSSkill sHSSkill;
